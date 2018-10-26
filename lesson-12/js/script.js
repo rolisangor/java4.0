60de95f44d252c16869185976a17584537946b05
Vitalii <siliiura@gmail.com>
2018-10-27 01:21:25 +0300

    добавлен 12 урок

--- lesson-12/js/script.js
+++ lesson-12/js/script.js
@@ -120,113 +120,99 @@ window.addEventListener('DOMContentLoaded', function () {
             }
     
     
-        // Отправка Модальные формы
-    
-        let message = {
-            loading: "Загрузка...",
-            success: "Спасибо! Скоро мы с вами свяжемся!",
-            failure: "Что-то пошло не так..."
-        };
-    
-        let form = document.querySelector('.main-form'),
-            input = form.getElementsByTagName('input')[0],
-            statusMessage = document.createElement('div');
-    
-            statusMessage.classList.add('status');
-    
-            input.addEventListener('keypress', function(e){
-                let key = e.keyCode;
-                 
-                if(key != 43 && key < 47 || key > 57) {
-                 e.preventDefault(); 
-                 
-                }
-         });
-            
-    
-        form.addEventListener('submit',function(event) {
+    // Отправка Модальные формы
+
+    let message = {
+        loading: "Загрузка...",
+        success: "Спасибо! Скоро мы с вами свяжемся!",
+        failure: "Что-то пошло не так..."
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input')[0],
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    input.addEventListener('keypress', function (e) {
+        let key = e.keyCode;
+        if (key != 43 && key < 47 || key > 57) {
+            e.preventDefault();
+
+        }
+    });
+
+    //  Отправка контактной формы
+
+    let formContainer = document.querySelector('.contact-form'),
+        contactForm = formContainer.getElementsByTagName('form')[0],
+        contactInput = contactForm.getElementsByTagName('input');
+
+    contactInput[1].addEventListener('keypress', function (e) {
+        let key = e.keyCode;
+        if (key != 43 && key < 47 || key > 57) {
+            e.preventDefault();
+
+        }
+    });
+
+
+    function sendForm(item) {
+
+        item.addEventListener('submit', function (event) {
             event.preventDefault();
-            form.appendChild(statusMessage);
-    
-            
-    
-            let request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
-                request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-    
-            let formData = new FormData(form);
-    
-            let obj = {};
-                formData.forEach(function(value, key) {
-                obj[key] = value;
-            });
-    
-            let json = JSON.stringify(obj);
-                request.send(json);
-    
-            request.addEventListener('readystatechange', function() {
-                if(request.readyState < 4) {
-                    statusMessage.innerHTML = message.loading;
-                } else if (request.readyState === 4 && request.status == 200) {
-                    statusMessage.innerHTML = message.success;
-                }else {
-                    statusMessage.innerHTML = message.failure;
-                }
-            });
-    
-            for(let i = 0; i < input.length; i++) {
-                input[i].value = '';
+            item.appendChild(statusMessage);
+
+            let formData = new FormData(item);
+
+            function postData(data) {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+
+
+                    let obj = {};
+                    formData.forEach(function (value, key) {
+                        obj[key] = value;
+                    });
+
+                    let json = JSON.stringify(obj);
+                    request.send(json);
+
+                    request.addEventListener('readystatechange', function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+
+
+
             }
-        });
-    
-    
-    
-        //  Отправка контактной формы
-    
-        let formContainer = document.querySelector('.contact-form'),
-            contactForm = formContainer.getElementsByTagName('form')[0],
-            contactInput = contactForm.getElementsByTagName('input');
-    
-            contactInput[1].addEventListener('keypress', function(e){
-                let key = e.keyCode;
-                 
-                if(key != 43 && key < 47 || key > 57) {
-                 e.preventDefault(); 
-                 
+            function clear() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
                 }
-         });
-    
-            contactForm.addEventListener('submit', function (event) {
-                event.preventDefault();
-            contactForm.appendChild(statusMessage);
-    
-            let myRequest = new XMLHttpRequest();
-            myRequest.open('POST', 'server.php');
-            myRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    
-            let formData = new FormData(contactForm);
-    
-            let obj = {};
-            formData.forEach(function (value, key) {
-                obj[key] = value;
-            });
-            let json = JSON.stringify(obj);
-            myRequest.send(json);
-    
-            myRequest.addEventListener('readystatechange', function() {
-                if(myRequest.readyState < 4) {
-                    statusMessage.innerHTML = message.loading;
-                } else if (myRequest.readyState === 4 && myRequest.status == 200) {
-                    statusMessage.innerHTML = message.success;
-                }else {
-                    statusMessage.innerHTML = message.failure;
-                }
-            });
-    
-            for (let i = 0; i < contactInput.length; i++) {
-                contactInput[i].value = '';
             }
-    
+
+            postData(formData)
+                .then(() => (statusMessage.innerHTML = message.loading))
+                .then(() => (statusMessage.innerHTML = message.success))
+                .catch(() => (statusMessage.innerHTML = message.failure))
+                .then(clear);
+
+
         });
-    
+    }
+
+    sendForm(form);
+    sendForm(contactForm);
+
+
     });
