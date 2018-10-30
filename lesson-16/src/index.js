f35397acc5d3075e179abbe294918c21bef29698
Vitalii <siliiura@gmail.com>
2018-10-30 22:05:29 +0200

    add lesson 16

---
+++ lesson-16/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './App';
+import Button from './Button';
+import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div className="wrapper">
+            <Clock />
+            <Button />
+         </div>
+      )
+   }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
