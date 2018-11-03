c9d9382ebbda219fa56c7e79947601a8ec4bc190
Vitalii <siliiura@gmail.com>
2018-11-03 04:59:51 +0200

    add Timer

--- FinalProject/src/js/parts/timer.js
+++ FinalProject/src/js/parts/timer.js
@@ -1,27 +1,30 @@
-function timer () {
+ function timer () {
    let deadline = '2019-07-04';
     
         let getTimeRemaining = (endtime) => {
             let t = Date.parse(endtime) - Date.parse(new Date()),
                 seconds = Math.floor((t/1000) % 60),
                 minutes = Math.floor((t/1000/60) % 60),
-                hours = Math.floor((t/(1000*60*60))); 
+                hours = Math.floor((t/1000/60/60 % 24)),
+                days = Math.floor((t/(1000*60*60*24)));
                 
             return {
                 'total' : t,
                 'hours' : hours,
                 'minutes' : minutes,
-                'seconds' : seconds
+                'seconds' : seconds,
+                'days' : days
             };
                 
             
         };
     
-        let setClock = (id, endtime) => {
-            let timer = document.getElementById(id),
+        let setClock = (endtime) => {
+            let timer = document.querySelector('.eTimer'),
                 hours = timer.querySelector('.hours'),
                 minutes = timer.querySelector('.minutes'),
                 seconds = timer.querySelector('.seconds'),
+                days = timer.querySelector('.days'),
                 timeInterval = setInterval(updateClock, 1000);
     
             function updateClock() {
@@ -40,6 +43,7 @@ function timer () {
                     hours.textContent = addZero(t.hours);
                     minutes.textContent = addZero(t.minutes);
                     seconds.textContent = addZero(t.seconds);
+                    days.textContent = addZero(t.days);
     
                     if(t.total <= 0) {
                         clearInterval(timeInterval);
@@ -47,11 +51,12 @@ function timer () {
                         hours.textContent = addZero(0);
                         minutes.textContent = addZero(0);
                         seconds.textContent = addZero(0);
+                        days.textContent = addZero(0);
                     }
             }
         };
     
-        setClock('timer', deadline);
+        setClock(deadline);
 }
 
-module.exports = timer;
+module.exports = timer; 
--- FinalProject/src/js/script.js
+++ FinalProject/src/js/script.js
@@ -5,11 +5,13 @@ window.addEventListener('DOMContentLoaded', function() {
        sendForm = require('./parts/form.js'),
        popupCalc = require('./parts/popup_calc.js'),
        tabsGlass = require('./parts/tabs.js'),
-       decorationTabs = require('./parts/decoration_tabs.js');
+       decorationTabs = require('./parts/decoration_tabs.js'),
+       timer = require('./parts/timer.js');
 
    modal();
    sendForm();
    popupCalc();
    tabsGlass();
    decorationTabs();
+   timer();
 });
